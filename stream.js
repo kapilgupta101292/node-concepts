d7b843827f0b4a52b56f9d716836e6f089924b4a
Kapil Gupta <kapilguptansit@gmail.com>
2022-03-05 11:21:04 +0530

    tried streams

--- stream.js
+++ stream.js
@@ -2,8 +2,33 @@ const fs = require('fs');
 const server = require('http').createServer();
 
 server.on('request', (req, res) => {
-	fs.readFile('test-file.txt', (err, data) => {
-		if (err) console.log(err);
-		res.end(data);
-	});
+	// Solution - 1
+	// fs.readFile('test-file.txt', (err, data) => {
+	// 	if (err) console.log(err);
+	// 	res.end(data);
+	// });
+
+	// Solution - 2 Streams
+	// const readable = fs.createReadStream('test-file.txt');
+	// readable.on('data', (chunk) => {
+	// 	res.write(chunk);
+	// });
+
+	// readable.on('end', () => {
+	// 	res.end();
+	// });
+
+	// readable.on('error', (err) => {
+	// 	console.error(err);
+	// 	res.statusCode = 500;
+	// 	res.end('There was an error');
+	// });
+
+	// Solution 3:
+	const readable = fs.createReadStream('test-file.txt');
+	readable.pipe(res);
+});
+
+server.listen(8000, 'localhost', () => {
+	console.log('Listening ...');
 });
